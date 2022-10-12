915346d9728378fe0d6266ef326f6180fdd070cf
Jihoon <35093963+MightyChipmunk@users.noreply.github.com>
2022-10-12 17:32:20 +0900

    .

--- Assets/Scripts/Deco_ChangeView.cs
+++ Assets/Scripts/Deco_ChangeView.cs
@@ -12,7 +12,7 @@ public class Deco_ChangeView : MonoBehaviour
         Third_Demen,
         First,
     }
-    
+    [SerializeField]
     ViewState _viewState;
     public ViewState viewState
     {
@@ -36,45 +36,67 @@ public class Deco_ChangeView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log(touch.position);
+                // 터치한 곳으로 Ray를 쏴서 그곳으로 배치
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 1000f))
+                {
+                    FirstPos = hit.transform.position;
+                }
+            }
+        }
         
     }
 
     public void On2DClicked()
     {
+        StopAllCoroutines();
         viewState = ViewState.Second_Demen;
     }
 
     public void On3DClicked()
     {
+        StopAllCoroutines();
         viewState = ViewState.Third_Demen;
     }
 
     public void OnFirstClicked()
     {
+        StopAllCoroutines();
         if (viewState == ViewState.First)
             return;
         else if (viewState == ViewState.Second_Demen)
+        {
             viewState = ViewState.Third_Demen;
+            Camera.main.transform.eulerAngles = new Vector3(90, 0, 0);
+            transform.position = new Vector3(transform.position.x, 15.0f, transform.position.z);
+        }
         else if (viewState == ViewState.Third_Demen)
             StartCoroutine("OnPosClick");
     }
 
     IEnumerator OnPosClick()
     {
-        int touchCount = Input.touchCount;
         while (true)
         {
-            if (touchCount < Input.touchCount)
+            if (Input.touchCount > 0)
             {
-                Touch touch = Input.GetTouch(touchCount - 1);
+                Touch touch = Input.GetTouch(0);
                 if (touch.phase == TouchPhase.Ended)
                 {
+                    Debug.Log(touch.position);
                     // 터치한 곳으로 Ray를 쏴서 그곳으로 배치
                     Ray ray = Camera.main.ScreenPointToRay(touch.position);
                     RaycastHit hit;
                     if (Physics.Raycast(ray, out hit, 30f, LayerMask.NameToLayer("Floor"))) 
                     {
-                        FirstPos = hit.point + Vector3.up * 1.5f;
+                        FirstPos = hit.transform.position;
                     }
                     viewState = ViewState.First;
                     break;
