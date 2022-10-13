cf9c544fd0c62edb8937d6db774b2f9efe5debd9
Jihoon <21qqq4@gmail.com>
2022-10-13 22:53:19 +0900

    물건 배치 구현

---
+++ Assets/Scripts/Deco_ObjectCol.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deco_ObjectCol : MonoBehaviour
+{
+    bool isCollide = false;
+    public bool IsCollide
+    {
+        get { return isCollide; }
+        private set { isCollide = value; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Floor"))
+            IsCollide = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Floor"))
+            IsCollide = false;
+    }
+}
--- Assets/Scripts/Deco_PutObject.cs
+++ Assets/Scripts/Deco_PutObject.cs
@@ -8,10 +8,12 @@ public class Deco_PutObject : MonoBehaviour
 
     public GameObject objFactory;
     GameObject obj;
-
+    bool canPut = true;
     public Material can;
     public Material cant;
     Material origMat;
+    Renderer objRenderer;
+    Deco_ObjectCol objCol;
 
     // Start is called before the first frame update
     void Start()
@@ -31,9 +33,10 @@ public class Deco_PutObject : MonoBehaviour
                 if (Physics.Raycast(ray, out hit, 16f, LayerMask.GetMask("Floor")))
                 {
                     obj = Instantiate(objFactory);
+                    objRenderer = obj.GetComponentInChildren<Renderer>();
                     obj.transform.parent = transform;
-                    //obj.transform.position = hit.point + obj.GetComponent<Collider>().;
-                    //origMat
+                    objCol = obj.transform.GetChild(0).gameObject.AddComponent<Deco_ObjectCol>();
+                    origMat = objRenderer.material;
                 }
             }
             else if (Input.GetKey(KeyCode.G))
@@ -41,21 +44,29 @@ public class Deco_PutObject : MonoBehaviour
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
                 if (Physics.Raycast(ray, out hit, 16f, LayerMask.GetMask("Floor")))
-                {
                     obj.transform.position = hit.point;
-                }
+                
                 if (Input.GetKey(KeyCode.Q))
-                {
-                    obj.transform.Rotate(0, -50f * Time.deltaTime, 0);
-                }
+                    obj.transform.Rotate(0, -100f * Time.deltaTime, 0);
                 else if (Input.GetKey(KeyCode.E))
-                {
-                    obj.transform.Rotate(0, 50f * Time.deltaTime, 0);
-                }
+                    obj.transform.Rotate(0, 100f * Time.deltaTime, 0);
+
+                canPut = !objCol.IsCollide;
+                if (canPut)
+                    objRenderer.material = can;
+                else
+                    objRenderer.material = cant;
             }
-            else if (Input.GetKeyUp(KeyCode.G))
+            else if (Input.GetKeyUp(KeyCode.G) && canPut)
             {
+                objRenderer.material = origMat;
+                obj.GetComponentInChildren<Collider>().isTrigger = false;
                 obj.transform.parent = room;
+                obj = null;
+            }
+            else if (Input.GetKeyUp(KeyCode.G) && !canPut)
+            {
+                Destroy(obj);
                 obj = null;
             }
         }
