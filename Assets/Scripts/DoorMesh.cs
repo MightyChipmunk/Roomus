ee8051b7e6f6343395a324778bda21dbe4380201
Jihoon <35093963+MightyChipmunk@users.noreply.github.com>
2022-10-18 18:38:27 +0900

    벽구멍

--- Assets/Scripts/DoorMesh.cs
+++ Assets/Scripts/DoorMesh.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WindowMesh : MonoBehaviour
+public class DoorMesh : MonoBehaviour
 {
-    Vector3[] verts = new Vector3[16];
+    Vector3[] verts = new Vector3[8];
 
     int[] tris = {
          0,1,4, 1,5,4, 1,2,5, 2,6,5,
          2,3,6, 3,7,6, 3,0,7, 0,4,7,
-         8,9,12, 9,13,12, 9,10,13, 10,14,13,
-         10,11,14, 11,15,14, 11,8,15, 8,12,15
+         //7,4,0, 7,0,3, 6,7,3, 6,3,2,
+         //5,6,2, 5,2,1, 4,5,1, 4,1,0,
      };
 
     void Start()
@@ -25,15 +25,6 @@ public class WindowMesh : MonoBehaviour
         verts[5] = + 0.25f * transform.right + 0.25f * transform.up;
         verts[6] = + 0.25f * transform.right - 0.25f * transform.up;
         verts[7] = - 0.25f * transform.right - 0.25f * transform.up;
-
-        verts[8] = + 0.5f * transform.right + 0.5f * transform.up;
-        verts[9] = - 0.5f * transform.right + 0.5f * transform.up;
-        verts[10] = - 0.5f * transform.right - 0.5f * transform.up;
-        verts[11] = + 0.5f * transform.right - 0.5f * transform.up;
-        verts[12] = +0.25f * transform.right + 0.25f * transform.up;
-        verts[13] = -0.25f * transform.right + 0.25f * transform.up;
-        verts[14] = -0.25f * transform.right - 0.25f * transform.up;
-        verts[15] = +0.25f * transform.right - 0.25f * transform.up;
 
         MeshFilter mF = gameObject.GetComponent<MeshFilter>(); // as MeshFilter;
         Mesh msh = new Mesh();
