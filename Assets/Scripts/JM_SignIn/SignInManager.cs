fc84a508c2fd74159006e998935ce8f98bf37f89
p2hminn <p2hminn@gmail.com>
2022-10-24 09:52:57 +0900

    로그인 및 회원가입 작업중

---
+++ Assets/Scripts/JM_SignIn/SignInManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignInManager : MonoBehaviour
+{
+    // ID
+    public InputField id;
+    // PW
+    public InputField pw;
+
+    string idInput;
+    string pwInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        id.onSubmit.AddListener(SubmitID);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void typeID(string input)
+    {
+        idInput = input;
+    }
+
+    void SubmitID(string idInput)
+    {
+
+    }
+
+    void SubmitPW(string pwInput)
+    {
+
+    }
+}
