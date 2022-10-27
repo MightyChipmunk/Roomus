b0079f3dfa5f3b8435df3b9334923c9a207ca5ed
p2hminn <p2hminn@gmail.com>
2022-10-27 13:06:54 +0900

    ui 작업중

---
+++ Assets/Scripts/JM_ToolBar.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JM_ToolBar : MonoBehaviour
+{
+    public GameObject toolBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            ActivateToolBar();
+        }
+
+        else if (Input.GetKeyUp(KeyCode.Alpha0))
+        {
+            DeactivateToolBar();
+        }      
+    }
+
+    void ActivateToolBar()
+    {
+        toolBar.transform.position = Input.mousePosition;
+        toolBar.SetActive(true);
+    }
+
+    void DeactivateToolBar()
+    {
+        toolBar.SetActive(false);
+    }
+
+    public void OnClickHome()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OnClickBack()
+    {
+
+    }
+
+    public void OnClickMyPage()
+    {
+
+    }
+     
+    public void OnClickTravel()
+    {
+
+    }
+
+    public void OnClickSettings()
+    {
+
+    }
+
+}
