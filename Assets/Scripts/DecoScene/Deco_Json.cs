1b1cb268e3ee92d741b49b3b84c036c9a79743e0
Jihoon <35093963+MightyChipmunk@users.noreply.github.com>
2022-11-02 17:12:15 +0900

    Trilib을 이용한 방 불러오기 구현

--- Assets/Scripts/DecoScene/Deco_Json.cs
+++ Assets/Scripts/DecoScene/Deco_Json.cs
@@ -42,8 +42,9 @@ public class Deco_Json : MonoBehaviour
 
     public static Deco_Json Instance { get; set; }
     ArrayJson arrayJson;
+    ArrayJson arrayJsonLoad;
 
-    public Objects objects;
+    Dictionary<Vector3, GameObject> objs = new Dictionary<Vector3, GameObject>();
 
     private void Awake()
     {
@@ -160,7 +161,7 @@ public class Deco_Json : MonoBehaviour
         //mapData.txt를 불러오기
         string jsonData = File.ReadAllText(Application.dataPath + "/RoomInfo" + "/" + roomName + ".txt");
         //ArrayJson 형태로 Json을 변환
-        ArrayJson arrayJson = JsonUtility.FromJson<ArrayJson>(jsonData);
+        arrayJsonLoad = JsonUtility.FromJson<ArrayJson>(jsonData);
         //ArrayJson의 데이터로 방 생성
         Destroy(GameObject.Find("Room"));
         GameObject newRoom = new GameObject("Room");
@@ -172,17 +173,21 @@ public class Deco_Json : MonoBehaviour
         newWalls.transform.position = Vector3.zero;
         newWalls.transform.rotation = Quaternion.identity;
         newWalls.transform.localScale = Vector3.one;
-        Deco_RoomInit.Instance.MakeRoom(arrayJson.XSize, arrayJson.YSize, arrayJson.ZSize, arrayJson.balcony, newRoom.transform);
-        SaveRoomInfo(roomName, arrayJson.XSize, arrayJson.YSize, arrayJson.ZSize, arrayJson.balcony);
+        Deco_RoomInit.Instance.MakeRoom(arrayJsonLoad.XSize, arrayJsonLoad.YSize, arrayJsonLoad.ZSize, arrayJsonLoad.balcony, newRoom.transform);
+        SaveRoomInfo(roomName, arrayJsonLoad.XSize, arrayJsonLoad.YSize, arrayJsonLoad.ZSize, arrayJsonLoad.balcony);
         //ArrayJson의 데이터를 가지고 오브젝트 생성
-        for (int i = 0; i < arrayJson.datas.Count; i++)
+        for (int i = 0; i < arrayJsonLoad.datas.Count; i++)
         {
-            SaveJsonInfo info = arrayJson.datas[i];
+            SaveJsonInfo info = arrayJsonLoad.datas[i];
             LoadObject(info.idx, info.position, info.eulerAngle, info.localScale, newRoom.transform);
         }
     }
     void LoadObject(int idx, Vector3 position, Vector3 eulerAngle, Vector3 localScale, Transform room)
     {
+        // id를 보냄
+        // id에 해당하는 fbx 파일과 Json파일을 받음
+        // 받은 정보와 포지션, 앵글, 스케일 값을 이용해서 생성
+
         DirectoryInfo di = new DirectoryInfo(Application.dataPath + "/LocalServer");
         foreach (FileInfo file in di.GetFiles())
         {
@@ -191,74 +196,106 @@ public class Deco_Json : MonoBehaviour
                 FBXJson fbxJson = JsonUtility.FromJson<FBXJson>(File.ReadAllText(file.FullName));
                 if (fbxJson.id == idx)
                 {
-                    foreach (FileInfo info in di.GetFiles())
-                    {
-                        if (info.Name.Contains(fbxJson.furnitName) && !info.Name.Contains("meta") && !info.Name.Contains("txt"))
-                        {
-                            byte[] data = File.ReadAllBytes(info.FullName);
-                            string path = Application.dataPath + "/Resources/" + info.Name;
-                            File.WriteAllBytes(path, data);
-
-                            StartCoroutine(WaitForUpload(info, fbxJson, idx, position, eulerAngle, localScale, room));
-                        }
-                    }
-
+                    StartCoroutine(WaitForFile(position, file.FullName.Substring(0, file.FullName.Length - 4) + ".fbx", position, eulerAngle, localScale, room, fbxJson));
                     //var assetLoaderOptions = AssetLoader.CreateDefaultLoaderOptions();
-                    //AssetLoader.LoadModelFromFile(file.FullName, OnLoad, OnMaterialsLoad, OnProgress, OnError, null, assetLoaderOptions);
+                    //AssetLoader.LoadModelFromFile(file.FullName.Substring(0, file.FullName.Length - 4) + ".fbx", OnLoad, OnMaterialsLoad, OnProgress, OnError, null, assetLoaderOptions);
                 }
             }
         }
     }
 
-    IEnumerator WaitForUpload(FileInfo file, FBXJson fbxJson, int idx, Vector3 position, Vector3 eulerAngle, Vector3 localScale, Transform room)
+    IEnumerator WaitForFile(Vector3 pos, string path, Vector3 position, Vector3 eulerAngle, Vector3 localScale, Transform room, FBXJson fbxJson)
     {
-        string path = file.Name.Substring(0, file.Name.Length - 4);
+        var assetLoaderOptions = AssetLoader.CreateDefaultLoaderOptions();
+        AssetLoader.LoadModelFromFile(path, OnLoad, OnMaterialsLoad, OnProgress, OnError, null, assetLoaderOptions);
 
-        while (true)
+        while (!objs.ContainsKey(pos))
         {
-            if (Resources.Load<GameObject>(path))
-                break;
-
             yield return null;
         }
 
-        if (path == fbxJson.furnitName)
+        GameObject obj = objs[pos];
+        obj.transform.parent = room;
+        obj.transform.localPosition = position;
+        obj.transform.localEulerAngles = eulerAngle;
+        obj.transform.localScale = localScale;
+        GameObject go = obj.transform.GetChild(0).gameObject;
+        BoxCollider col = go.AddComponent<BoxCollider>();
+        col.center = new Vector3(0, fbxJson.ySize / 2, 0);
+        col.size = new Vector3(fbxJson.xSize, fbxJson.ySize, fbxJson.zSize);
+        Rigidbody rb = go.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        if (fbxJson.location)
+            go.transform.localPosition = Vector3.zero + Vector3.forward;
+        else if (!fbxJson.location)
+            go.transform.localPosition = Vector3.zero + Vector3.forward * (fbxJson.zSize / 2 + 0.01f);
+        go.transform.localRotation = Quaternion.identity;
+        Deco_Idx decoIdx = obj.AddComponent<Deco_Idx>();
+        decoIdx.Name = fbxJson.furnitName;
+        decoIdx.Price = fbxJson.price;
+        decoIdx.Category = fbxJson.category;
+        decoIdx.Idx = fbxJson.id;
+
+        for (int i = 0; i < go.transform.childCount; i++)
         {
-            GameObject obj = new GameObject(fbxJson.furnitName);
-            obj.transform.parent = room;
-            obj.transform.localPosition = position;
-            obj.transform.localEulerAngles = eulerAngle;
-            obj.transform.localScale = localScale;
-            GameObject go = Instantiate(Resources.Load<GameObject>(fbxJson.furnitName));
-            BoxCollider col = go.AddComponent<BoxCollider>();
-            col.center = new Vector3(0, fbxJson.ySize / 2, 0);
-            col.size = new Vector3(fbxJson.xSize, fbxJson.ySize, fbxJson.zSize);
-            Rigidbody rb = go.AddComponent<Rigidbody>();
-            rb.useGravity = false;
-            rb.velocity = Vector3.zero;
-            go.transform.parent = obj.transform;
-            if (fbxJson.location)
-                go.transform.localPosition = Vector3.zero + Vector3.forward;
-            else if (!fbxJson.location)
-                go.transform.localPosition = Vector3.zero + Vector3.forward * (fbxJson.zSize / 2 + 0.01f);
-            go.transform.localEulerAngles = Resources.Load<GameObject>(fbxJson.furnitName).transform.eulerAngles;
-            Deco_Idx decoIdx = obj.AddComponent<Deco_Idx>();
-            decoIdx.Name = fbxJson.furnitName;
-            decoIdx.Price = fbxJson.price;
-            decoIdx.Category = fbxJson.category;
-            decoIdx.Idx = fbxJson.id;
-
-            for (int i = 0; i < go.transform.childCount; i++)
-            {
-                if (File.Exists(Application.dataPath + "/Resources/" + fbxJson.furnitName + "Tex" + i.ToString() + ".jpg"))
-                {
-                    go.transform.GetChild(i).GetComponent<Renderer>().material.mainTexture =
-                        Resources.Load<Texture>(fbxJson.furnitName + "Tex" + i.ToString());
-                }
-            }
-
-            SaveJson(obj, obj.GetComponent<Deco_Idx>().Idx);
+            go.transform.GetChild(i).GetComponent<Renderer>().material.shader = Shader.Find("Universal Render Pipeline/Lit");
         }
+
+        for (int i = 0; i < go.transform.childCount; i++)
+        {
+            if (File.Exists(Application.dataPath + "/LocalServer/" + fbxJson.furnitName + "Tex" + i.ToString() + ".jpg"))
+            {
+                Texture2D tex = new Texture2D(2, 2);
+                tex.LoadImage(File.ReadAllBytes(Application.dataPath + "/LocalServer/" + fbxJson.furnitName + "Tex" + i.ToString() + ".jpg"));
+                go.transform.GetChild(i).GetComponent<Renderer>().material.mainTexture = tex;
+            }
+        }
+
+        SaveJson(obj, obj.GetComponent<Deco_Idx>().Idx);
     }
+
+    #region Trilib
+    /// <summary>
+    /// Called when any error occurs.
+    /// </summary>
+    /// <param name="obj">The contextualized error, containing the original exception and the context passed to the method where the error was thrown.</param>
+    private void OnError(IContextualizedError obj)
+    {
+        Debug.LogError($"An error occurred while loading your Model: {obj.GetInnerException()}");
+    }
+
+    /// <summary>
+    /// Called when the Model loading progress changes.
+    /// </summary>
+    /// <param name="assetLoaderContext">The context used to load the Model.</param>
+    /// <param name="progress">The loading progress.</param>
+    private void OnProgress(AssetLoaderContext assetLoaderContext, float progress)
+    {
+        Debug.Log($"Loading Model. Progress: {progress:P}");
+    }
+
+    int objIdx = 0;
+    /// <summary>
+    /// Called when the Model (including Textures and Materials) has been fully loaded.
+    /// </summary>
+    /// <remarks>The loaded GameObject is available on the assetLoaderContext.RootGameObject field.</remarks>
+    /// <param name="assetLoaderContext">The context used to load the Model.</param>
+    private void OnMaterialsLoad(AssetLoaderContext assetLoaderContext)
+    {
+        Debug.Log("Materials loaded. Model fully loaded.");
+        objs.Add(arrayJsonLoad.datas[objIdx++].position, assetLoaderContext.RootGameObject);
+    }
+
+    /// <summary>
+    /// Called when the Model Meshes and hierarchy are loaded.
+    /// </summary>
+    /// <remarks>The loaded GameObject is available on the assetLoaderContext.RootGameObject field.</remarks>
+    /// <param name="assetLoaderContext">The context used to load the Model.</param>
+    private void OnLoad(AssetLoaderContext assetLoaderContext)
+    {
+        Debug.Log("Model loaded. Loading materials.");
+    }
+    #endregion
 }
 
